e486276f54d746b7eba287a17179927e8627677a
takaza-codes <elekovalenko@gmail.com>
2025-03-01 21:35:52 +0300

    update js, checkbox style

--- main.js
+++ main.js
@@ -3,22 +3,64 @@ const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 const noTasksMsg = document.getElementById('noTasks');
 const clearButton = document.getElementById('clearButton');
+const errorMsg = document.getElementById('errorMessage');
 
-addButton.addEventListener('click', function () {
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+document.addEventListener('DOMContentLoaded', prevTasks);
+
+function prevTasks() {
+    taskList.innerHTML = '';
+    if (tasks.length === 0) {
+        noTasksMsg.innerText = 'No tasks';
+        clearButton.setAttribute('disabled', '');
+    } else {
+        noTasksMsg.innerText = '';
+        clearButton.removeAttribute('disabled');
+    }
+
+    tasks.forEach(() => {
+        const newElement = document.createElement('li');
+        newElement.innerHTML = `${taskInput.value.trim()} <input type="checkbox" class="taskCheck" name="taskCheck" unchecked />`;
+        taskList.append(newElement);
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+function checkInput() {
+    if (taskInput.value.trim() === '') {
+        errorMsg.innerText = 'Enter your task';
+    return;
+} else {
     noTasksMsg.innerText = '';
-    clearButton.removeAttribute('disabled');
+
     const newElement = document.createElement('li');
-    newElement.innerHTML = `${taskInput.value} <input type="checkbox" id="taskCheck" name="taskCheck" unchecked />`;
-    taskInput.value = '';
+    errorMsg.innerText = '';
+    newElement.innerHTML = `${taskInput.value.trim()} <input type="checkbox" class="taskCheck" name="taskCheck" unchecked />`;
     taskList.append(newElement);
+    tasks.push(taskInput.value);
+    clearButton.removeAttribute('disabled');
+}
+}
+
+
+taskList.addEventListener('click', (event) => {
+    if (event.target.tagName == 'LI') {
+        if (event.target.classList.contains('done')) {
+            event.target.classList.remove('done');
+        } else {event.target.classList.add('done')}
+    } 
 });
 
-const list = document.getElementById('taskList');
-const taskCheckbox = document.getElementById('taskCheck');
-list.addEventListener('click', function (event) {
-	if (event.target.tagName === 'LI') {
-		event.target.classList.toggle('done');
-        taskCheckbox.removeAttribute('unchecked'); //cannot read properties of null
-        taskCheckbox.addAttribute('checked');
-	}
+addButton.addEventListener('click', function () {
+    checkInput();
+    taskInput.value = '';
+});
+
+clearButton.addEventListener('click', function () {
+    taskList.innerHTML = '';
+    tasks = [];
+    clearButton.setAttribute('disabled', '');
 });
